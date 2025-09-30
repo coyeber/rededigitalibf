181e7a11c1c8438f7da46d2bbd01618fcaa7efd2
coyeber <sergiobjunior232@gmail.com>
2025-09-30 20:17:41 -0300

    Update sistema_ibf_js.js

--- sistema_ibf_js.js
+++ sistema_ibf_js.js
@@ -2,7 +2,7 @@
 // CONFIGURAÇÃO DO SUPABASE
 // ============================================
 // SUBSTITUA com suas credenciais do Supabase
-const SUPABASE_URL = 'https://mvxmpveipajedqowwwdo.supabase.co';
+const SUPABASE_URL = 'https://mvxmpveipajcdqowwwdo.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12eG1wdmVpcGFqZWRxb3d3d2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNjU0NzIsImV4cCI6MjA3NDg0MTQ3Mn0.Ve2u4J30aVP0pNQHZxd8Zuzl911I-8ClMwCe_NlZvA0';
 
 // Inicializar cliente Supabase
